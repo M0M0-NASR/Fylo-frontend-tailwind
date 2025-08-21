35d1855d30170de387e5eec38f29099bef40583e
M0M0-NASR <mohamednasrmahmood@gmail.com>
2025-08-21 11:21:11 +0300

    Intiailze Projcet

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from "react";
+
+import "../App.css";
+
+function Header() {
+  const [links, setLinks] = useState([
+    { id: 1, name: "Features", url: "/features" },
+    { id: 2, name: "Teams", url: "/teams" },
+    { id: 3, name: "Sign in", url: "/signin" },
+  ]);
+
+  const headerRef = useRef();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        headerRef.current.style.background = "#171f2a";
+        headerRef.current.style.padding = "20px 0";
+      } else {
+        headerRef.current.style.background = "transparent";
+        headerRef.current.style.padding = "60px 0";
+      }
+    });
+  }, [headerRef]);
+
+  return (
+    <header
+      ref={headerRef}
+      className="w-full top-0 z-50 py-[50px] transition-all duration-200 fixed bg-transparent"
+    >
+      <div className="container mx-auto flex flex-col items-center sm:flex-row sm:justify-between">
+        <div className="logo">
+          <img src="/src/assets/imgs/logo.svg" alt="Logo" />
+        </div>
+        <ul className="flex items-center gap-[50px] text-white ">
+          {links.map((link) => (
+            <li key={link.id}>
+              <a
+                href={`${link.url}`}
+                className="text-white opacity-[0.8] hover:opacity-[1] hover:underline transition-opacity duration-300 p-[20px]"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
+}
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <section className="home py-[150px]">
+      <div className="container flex flex-col justify-center items-center">
+        <div>
+          <img src="/src/assets/imgs/illustration-intro.png"></img>
+        </div>
+        <h4>
+          All your files in one secure location,
+          <br></br>
+          accessible anywhere.
+        </h4>
+        <p>
+          Fylo stores all your most important files in one secure location.
+          <br></br>
+          Access them wherever you need, share and collaborate with
+          <br></br>
+          friends , family and co-workers.
+        </p>
+      </div>
+    </section>
+  );
+}
+export default Home;
