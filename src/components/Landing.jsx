3d6998fad330e86208c73f5d02a25f7296056843
M0M0-NASR <mohamednasrmahmood@gmail.com>
2025-08-27 09:24:22 +0300

    Fix Image Problem that cant show in deployment

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,11 @@
+import hero from "./src/assets/imgs/illustration-intro.png";
+
 function Home() {
   return (
     <section className="w-full home pt-[150px] ">
       <div className="container mx-auto flex flex-col sm:justify-center items-center">
         <div className="animate-updown ">
-          <img src="/src/assets/imgs/illustration-intro.png"></img>
+          <img src={hero}></img>
         </div>
         <h1 className=" text-[30px] font-bold py-[20px] text-center">
           All your files in one secure location,
@@ -20,7 +22,6 @@ function Home() {
         <button className="my-[20px] py-[12px] px-[100px] main-btn">
           Get Started
         </button>
-        
       </div>
     </section>
   );
